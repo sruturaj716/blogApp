af090de706ece3e33da4cbe630147f0fec89f862
Ruturaj Shinde <sruturaj716@gmail.com>
2024-04-07 21:33:29 +0530

    Categories list added at side-nav

--- src/app/shared/components/sidenav/sidenav.component.ts
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -5,15 +5,31 @@ import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChange
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss'],
 })
-export class SidenavComponent {
-
+export class SidenavComponent implements OnInit {
   @Input() openSideNav: boolean = false;
   @Output() closeSidenav: EventEmitter<boolean> = new EventEmitter();
 
   public setOpen: boolean = false;
+  public showCategories: boolean = false;
+  public arrowState: string = 'down';
+  public isClicked: boolean = false;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
 
   public handleBackdropClick() {
     this.closeSidenav.emit(false);
+    this.showCategories = false;
+    this.isClicked = false;
+    this.arrowState = 'down';
   }
 
+  public onCategories() {
+    this.showCategories = !this.showCategories;
+    this.isClicked = true;
+    this.arrowState = this.arrowState === 'down' ? 'up' : 'down';
+  }
 }
