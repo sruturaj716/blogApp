b176553cba3210a503f4a1423bc04e03cf45ccaf
Ruturaj Shinde <sruturaj716@gmail.com>
2024-05-26 09:00:22 +0530

    image added at post-scroll

--- src/app/features/home/post-scroll/post-scroll.component.ts
+++ src/app/features/home/post-scroll/post-scroll.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import data from 'src/app/shared/data/home-card-data.json'
 
 @Component({
@@ -7,13 +7,21 @@ import data from 'src/app/shared/data/home-card-data.json'
   styleUrls: ['./post-scroll.component.scss']
 })
 export class PostScrollComponent implements OnInit {
-  @ViewChild('postCard') postCard!: ElementRef;
-
   public postInfo: any[] = [];
-  constructor() { }
+  public postImages: any[] = [];
+  public currentPostImage: string = '';
+
+  constructor() {
+    this.postImages = [
+      { id: 0, img: 'assets/post_lg_1.jpg.webp' },
+      { id: 1, img: 'assets/post_lg_2.jpg.webp' },
+      { id: 2, img: 'assets/post_lg_3.jpg.webp' },
+      { id: 3, img: 'assets/post_lg_4.jpg.webp' }];
+  }
 
   ngOnInit(): void {
     this.postInfo = [data.homeSliderCardData[0]];
+    this.currentPostImage = this.postImages[0].img.toString();
   }
 
   public OnSlide(value: string, item: any) {
@@ -21,9 +29,15 @@ export class PostScrollComponent implements OnInit {
     let newIndex: number;
     if (value === 'right') {
       newIndex = (currentIndex + 1) % data.homeSliderCardData.length;
+      this.currentPostImage = this.getCurrentPostimage(newIndex);
     } else {
       newIndex = (currentIndex - 1 + data.homeSliderCardData.length) % data.homeSliderCardData.length;
+      this.currentPostImage = this.getCurrentPostimage(newIndex);
     }
     this.postInfo = [data.homeSliderCardData[newIndex]];
+  }
+
+  public getCurrentPostimage(currentIndex: number): string {
+    return this.postImages.filter(images => images.id == currentIndex).map(image => image.img).toString();
   }
 }
