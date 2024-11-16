3e94df412decf6abde6d730c7b5f89804fe8329a
Ruturaj Shinde <sruturaj716@gmail.com>
2024-11-16 13:51:12 +0530

    Enable :- HashLocationStrategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })], // Enable HashLocationStrategy
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
